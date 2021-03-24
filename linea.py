c7790fd24c8edd60a5971de5412fb5583917a3d4
rtyui5 <itzcalamity666@gmail.com>
2021-03-24 17:12:54 +0100

    Primera entrega

---
+++ linea.py
@@ -0,0 +1,43 @@
+import math
+import punto
+
+class Linea:
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.pendiente = 0
+
+    def dospuntos(self,p1,p2):
+        self.representacion = "0 = [" + p2.get_y + "-" + p1.get_y +")/(" + p2.get_x + "-" + p1.get_x + ")]*(x-" + p1.get_x + ")+" + p1.get_y
+        self.pendiente = (p2.get_y-p1.get_y)/(p2.get_x-p1.get_x)
+
+    def pendienteordenada(self,m,b):
+        self.representacion = "0 = " + m + "*x+" + b + "- y"
+        self.pendiente = m
+
+    def pendientepunto(self,m,p1):
+        self.representacion = "0 = " + m + "*(x-" + p1.get_y + ")+" + p1.get_y + "- y"
+        self.pendiente = m
+
+    def coeficientes(self,a,b,c):
+        self.representacion = "0 = " + a + "*x+" + b + "*y+" + "c"
+        self.pendiente = -a/b
+
+    def vertical(self,x0):
+        self.representacion = "0 = " + x0 + "- x"
+        self.pendiente = 0
+
+    def horizontal(self,y0):
+        self.representacion = "0 = " + y0 + "- y"
+        self.pendiente = 0
+
+    def interseccion(self, linea):
+        if self.pendiente == linea.pendiente:
+            return 0
+        else:
+            return 1
+
+    def devolverrepresentacion(self):
+        return self.representacion
+
--- poligono.py
+++ poligono.py
@@ -1,46 +1,68 @@
-import vector
-import punto
+from vector import Vector
+from punto import Punto
 
 class Poligono:
 
-    def __init__(self, puntos):
-        self.puntos = puntos
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.aristas = len(vertices)
+
 
     def concavo(self):
         i=0
-        vec1 = vector.Vector(self.puntos[i], self.puntos[i+1])
-        vec2 = vector.Vector(self.puntos[i+1], self.puntos[i+2])
+        vec1 = Vector(self.vertices[i], self.vertices[i+1])
+        vec2 = Vector(self.vertices[i+1], self.vertices[i+2])
         prodvectorial = vec1.productovectorial(vec2)
         positivo = prodvectorial >=0
 
-        for i in range(0, self.puntos.size)
-            vec1 = vector.Vector(self.puntos[i], self.puntos[i+1])
-            vec2 = vector.Vector(self.puntos[i+1], self.puntos[i+2])
+        for i in range(0, len(self.vertices)):
+            vec1 = Vector(self.vertices[i], self.vertices[i+1])
+            vec2 = Vector(self.vertices[i+1], self.vertices[i+2])
             prodvectorial = vec1.productovectorial(vec2)
-            if(positivo != (prodvectorial >=0)
-                return true
+            flag = prodvectorial >=0
+            if positivo != flag:
+                return True
 
-        vec1 = vector.Vector(self.puntos[i+1], self.puntos[0])
+        vec1 = Vector(self.vertices[i+1], self.vertices[0])
         prodvectorial = vec2.productovectorial(vec1)
-        if(positivo != (prodvectorial >=0)
-            return true
-        return false
+        if positivo != (prodvectorial >=0):
+            return True
+        return False
 
     def centroide(self):
-        x,y,z=0.0
-
-        for k in range(0,self.puntos):
-            z+=self.puntos[k].get_x*self.puntos[(k+1)%self.puntos].get_y - self.puntos[k].get_y*self.puntos[(k+1)%self.puntos].get_x
+        x=0.0
+        y=0.0
+        z=0.0
+        for k in range(0,self.vertices):
+            z+=self.vertices[k].get_x*self.vertices[(k+1)%self.vertices].get_y - self.vertices[k].get_y*self.vertices[(k+1)%self.vertices].get_x
         z=z/2
 
-        for k in range(0,self.puntos):
-            x+=self.puntos[k].get_x*self.puntos[(k+1)%self.puntos].get_x*self.puntos[k].get_x*self.puntos[(k+1)%self.puntos].get_y - self.puntos[(k+1)%self.puntos].get_x*self.puntos[k].get_y
+        for k in range(0,self.vertices):
+            x+=self.vertices[k].get_x*self.vertices[(k+1)%self.vertices].get_x*self.vertices[k].get_x*self.vertices[(k+1)%self.vertices].get_y - self.vertices[(k+1)%self.vertices].get_x*self.vertices[k].get_y
         x=x/(6*z)
 
-        for k in range(0,self.puntos):
-            y+=self.puntos[k].get_x*self.puntos[(k+1)%self.puntos].get_x*self.puntos[k].get_x*self.puntos[(k+1)%self.puntos].get_y - self.puntos[(k+1)%self.puntos].get_x*self.puntos[k].get_y
+        for k in range(0,self.vertices):
+            y+=self.vertices[k].get_x*self.vertices[(k+1)%self.vertices].get_x*self.vertices[k].get_x*self.vertices[(k+1)%self.vertices].get_y - self.vertices[(k+1)%self.vertices].get_x*self.vertices[k].get_y
         y=y/(6*z)
+        return Punto(x,y)
 
-        return punto.Punto(x,y)
+    def triangular(self,posicion_vertice):
+        vertices = []
+        vertices.append(self.vertices[posicion_vertice-1])
+        vertices.append(self.vertices[posicion_vertice])
+        if posicion_vertice == (len(self.vertices)-1):
+            vertices.append(self.vertices[0])
+        else:
+            vertices.append(self.vertices[posicion_vertice+1])
+        return Poligono(vertices)
+    
+    def triangulacion(self,poligono):
+        i=0
+        triangulos = []
+        for i in range(0, len(self.vertices)):
+            triangulos.append(self.triangular(i))
+        return triangulos
+
+    
 
 
